36ee69667c0c6a065183c9027ddeec61ce312d41
Lairson Barreto de Melo Filho <lairsonm@gmail.com>
2017-11-13 00:17:03 -0200

    remoção de som do jogo

--- SpaceInvaders/spaceinvaders.py
+++ SpaceInvaders/spaceinvaders.py
@@ -198,27 +198,27 @@ class Mystery(sprite.Sprite):
         self.timer = time.get_ticks()
         #self.mysteryEntered = mixer.Sound('sounds/mysteryentered.wav')
         #self.mysteryEntered.set_volume(0.3)
-        self.playSound = True
+        #self.playSound = True
 
     def update(self, keys, currentTime, *args):
         resetTimer = False
-        if (currentTime - self.timer > self.moveTime) and (self.rect.x < 0 or self.rect.x > 800) and self.playSound:
-            self.mysteryEntered.play()
-            self.playSound = False
+        #if (currentTime - self.timer > self.moveTime) and (self.rect.x < 0 or self.rect.x > 800) and self.playSound:
+            #self.mysteryEntered.play()
+            #self.playSound = False
         if (currentTime - self.timer > self.moveTime) and self.rect.x < 840 and self.direction == 1:
-            self.mysteryEntered.fadeout(4000)
+            #self.mysteryEntered.fadeout(4000)
             self.rect.x += 2
             SCREEN.blit(self.image, self.rect)
         if (currentTime - self.timer > self.moveTime) and self.rect.x > -100 and self.direction == -1:
-            self.mysteryEntered.fadeout(4000)
+            #self.mysteryEntered.fadeout(4000)
             self.rect.x -= 2
             SCREEN.blit(self.image, self.rect)
         if (self.rect.x > 830):
-            self.playSound = True
+            #self.playSound = True
             self.direction = -1
             resetTimer = True
         if (self.rect.x < -90):
-            self.playSound = True
+            #self.playSound = True
             self.direction = 1
             resetTimer = True
         if (currentTime - self.timer > self.moveTime) and resetTimer:
@@ -538,7 +538,7 @@ class SpaceInvaders(object):
         if mysterydict:
             for value in mysterydict.values():
                 for currentSprite in value:
-                    currentSprite.mysteryEntered.stop()
+                    #currentSprite.mysteryEntered.stop()
                     #self.sounds["mysterykilled"].play()
                     score = self.calculate_score(currentSprite.row)
                     explosion = Explosion(currentSprite.rect.x, currentSprite.rect.y, currentSprite.row, False, True, score)
@@ -637,7 +637,7 @@ class SpaceInvaders(object):
                         self.gameTimer += 3000
                 else:
                     currentTime = time.get_ticks()
-                    self.play_main_music(currentTime)
+                    #self.play_main_music(currentTime)
                     SCREEN.blit(self.background, (0,0))
                     self.allBlockers.update(SCREEN)
                     self.scoreText2 = Text(FONT, 20, str(self.score), GREEN, 85, 5)
